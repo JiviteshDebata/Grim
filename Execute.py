49e21365412b407884b49ecd69a77db8244e0e52
Jivitesh Debata <jivitesh.debata@gmail.com>
2018-01-18 09:33:58 +0530

    Fixed Single syntax error

--- Execute.py
+++ Execute.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 #def download_image()
 #def preview_image()
 
+root=Tk()#main window
 root.title("GRIM")
 
 Fline=Label(root,text="This program modifies the grub file in order to function properly")
@@ -13,8 +14,9 @@ Sel_Img=Button(root ,text="Select Image")
 Dwn_Img=Button(root ,text="Download Image")
 Prv_Img=Button(root ,text="Preview Image")
 
+#Allignment
 Fline.grid(row=0,pady=2,padx=20,sticky=W)
-Sline.gride(row=1,stick=S)
+Sline.grid(row=1,stick=S)
 Sel_Img.grid(row=2,padx=20,stick=W)
 Dwn_Img.grid(row=2,padx=1,stick=E)
 Prv_Img.grid(row=3,column=0,padx=2,sticky=S)
