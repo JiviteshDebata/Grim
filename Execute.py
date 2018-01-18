83748171f691664652350073d68222932a868dea
Jivitesh Debata <jivitesh.debata@gmail.com>
2018-01-18 18:31:18 +0530

    Select image now works
    Feature 1/3 90% done
    problem is running this code twice will break grub. So its not prodution ready yet

--- Execute.py
+++ Execute.py
@@ -1,16 +1,32 @@
+import os
+import sys
+import subprocess
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
-#def select_image()
+
+def set_image(path):
+    to_write="GRUB_BACKGROUND="+path
+    f=open('/etc/default/grub','a+')
+    f.write(to_write)
+
+#main window
+root=Tk()
+root.title("GRIM")
+root.geometry("380x120")
+
+def select_image():
+    root.filename = filedialog.askopenfilename(title="Select file",filetypes = (("png","*.png"),("All files","*.*")))
+    path=root.filename
+    set_image(path)
+
 #def download_image()
 #def preview_image()
 
-root=Tk()#main window
-root.title("GRIM")
-
 Fline=Label(root,text="This program modifies the grub file in order to function properly")
 Sline=Label(root,text="Use at your own risk")
-Sel_Img=Button(root ,text="Select Image")
+Sel_Img=Button(root ,text="Select Image",command=lambda:select_image())
 Dwn_Img=Button(root ,text="Download Image")
 Prv_Img=Button(root ,text="Preview Image")
 
@@ -18,10 +34,7 @@ Prv_Img=Button(root ,text="Preview Image")
 Fline.grid(row=0,pady=2,padx=20,sticky=W)
 Sline.grid(row=1,stick=S)
 Sel_Img.grid(row=2,padx=20,stick=W)
-Dwn_Img.grid(row=2,padx=1,stick=E)
-Prv_Img.grid(row=3,column=0,padx=2,sticky=S)
+Dwn_Img.grid(row=2,padx=20,stick=E)
+Prv_Img.grid(row=3,column=0,padx=2,pady=10,sticky=S)
 
-#Event Handlers(Mouse Button 1 only)
-#Sel_Img.bind("<Button-1",select_image)
-#Dwn_Img.bind("<Button-1",download_image)
-#Prv_Img.bind("<Button-1",preview_image)
+root.mainloop()
