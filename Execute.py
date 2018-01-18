0eff42c207d003c7f3ca9a11d0d6a311d15ff262
Jivitesh Debata <jivitesh.debata@gmail.com>
2018-01-18 23:37:01 +0530

    Code snippets for Download

--- Execute.py
+++ Execute.py
@@ -11,13 +11,23 @@ from PIL import Image
 if os.geteuid() != 0:
     os.execvp("sudo", ["sudo"]+["python3"] + sys.argv) #To check root priviledges
 
+#Code below should remove the last line from GRUB if image has already been set
+#------------------------------------------------------------------------------
+readCurrent=open('/etc/default/grub','a+')
+lastLine=readCurrent.readlines()[:-1]
+readCurrent.close()
+if(lastLine[:15]=="GRUB_BACKGROUND="):
+    writeCurrent=open('/etc/default/grub','a+');
+    writeCurrent.writelines("")[:-1]
+    writeCurrent.close()
+#For some reason it doesnt.Needs to be fixed------------------------------------
 
 path=""
 def set_image(data):
     to_write="GRUB_BACKGROUND="+data
-    f=open('/etc/default/grub','a+')
-    f.write(to_write)
-    f.close()
+    writeCurrent=open('/etc/default/grub','a+')
+    writeCurrent.write(to_write)
+    writeCurrent.close()
 
 def select_image():
     global path
@@ -25,7 +35,16 @@ def select_image():
     path=root.filename
     set_image(path)
 
-#def download_image()
+def download_image():
+    window=Toplevel(root)
+    URL=Entry(window)
+    TAG=Entry(window)
+    Fline=Label(window,text="Enter URL").grid(row=0)
+    Sline=Label(window,text="Enter Tags").grid(row=1)
+    Tline=Label(window,text="Keep in mind to separate tags by spaces").grid(row=2,pady=10,sticky=S)
+    String Url=URL.get()
+    String Tag=tag.get()
+
 def preview_image():
     global path
     if(path==""):
@@ -44,7 +63,7 @@ root.geometry("420x120")
 Fline=Label(root,text="This program modifies the grub file in order to function properly")
 Sline=Label(root,text="Use at your own risk")
 Sel_Img=Button(root ,text="Select Image",command=lambda:select_image())
-Dwn_Img=Button(root ,text="Download Image")
+Dwn_Img=Button(root ,text="Download Image"command=lambda:download_image())
 Prv_Img=Button(root ,text="Preview Image",command=lambda:preview_image())
 
 #Allignment
